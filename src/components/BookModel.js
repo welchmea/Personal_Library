d6394d126f6dd46b8ec1aebe8ce7b4106802598d
Meagan <welchmea@oregonstate.edu>
2024-04-06 15:30:11 -0500

    changed styling on Search Page

--- src/components/BookModel.js
+++ src/components/BookModel.js
@@ -1,22 +1,20 @@
 import React from "react";
 import { Link } from 'react-router-dom';
-import { GrView } from 'react-icons/gr';
 
 // represents data for each individual item in the search results
 function BookModel (props) {
 
   return (
     <>
-    <figure className="images">
+    <figure className="images flex flex-col items-center border border-1 border-black mt-8 p-8 ml-8 font-normal gap-y-2">
     <img src={props.image} alt="" width={200} height={300}></img>
       <figcaption>
       <p>{props.title}</p>
       <p>{props.author}</p>
       </figcaption>
       <button className="main-button">
-      <Link to="/Books" state={props} className="viewButton"><i><GrView className="viewButton"/>More Info</i></Link><br/>
+      <Link to="/Books" state={props}>More Info</Link><br/>
       </button>
-      
     </figure>
 
     </>
