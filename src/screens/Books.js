fd217aa39cb4d70eed86a708e87f677c8b6ee6f8
Meagan <welchmea@oregonstate.edu>
2024-04-06 16:21:41 -0500

    Modified more info styling and CSS

--- src/screens/Books.js
+++ src/screens/Books.js
@@ -1,57 +1,79 @@
-import React, {useState } from "react";
+import React, { useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import AddLibrary from "../components/AddLibrary";
 import AddQueue from "../components/AddQueue";
-import { AiOutlineCheck, AiOutlinePlusCircle} from "react-icons/ai";
+import { AiOutlineCheck, AiOutlinePlusCircle } from "react-icons/ai";
 import AddBrowse from "../components/AddBrowse";
 import { Tooltip } from "@mui/material";
 
-function Books () {
-    
-    // variables for rendering different components, navigation, and retrieving info using state.
-    const [render, setRender] = useState(false);
-    const [renderQueue, setRenderQueue] = useState(false)
+function Books() {
+  // variables for rendering different components, navigation, and retrieving info using state.
+  const [render, setRender] = useState(false);
+  const [renderQueue, setRenderQueue] = useState(false);
 
-    const location = useLocation();
-    const result = location.state;
+  const location = useLocation();
+  const result = location.state;
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    //code on lines 24/25 taken from React navigation: url:https://reactnavigation.org/docs/navigation-prop/
-    const goBack = () => {
-		navigate(-1);
-	};
-    
-    return (
-        <>
-        <main>
-            <section>
-                <button className="main-button" onClick={goBack}> Back to Search Results</button>
-                <figure className="images">
-                    <img src={result.image} alt="" width={200} height={300}></img>
-                        <figcaption>
-                            <h2>{result.title}</h2>
-                            <h1>{result.author}</h1>
-                            <p>{result.description}</p>
-                            <p>Genre: {result.category}</p>
-                            <p>Page Count: {result.pageCount}</p>
-                            <p>Publication Date: {result.publishedDate}</p>
-                            <p>Publisher: {result.publisher}</p>
-                {/* <p>Cost on Barnes and Noble: {price}</p> */}
-                        </figcaption>
-                        <AddBrowse result={result}/>
-                        <Tooltip title="Add to Bookshelf">
-                            <button className="more-info-popout" onClick={() => setRender(true)} ><i><AiOutlineCheck className='icon-five'/></i></button>
-                            {render ? <AddLibrary result={result} />: null}
-                        </Tooltip>
-                        <Tooltip title="Add to Queue">
-                            <button className="more-info-popout" onClick={() => setRenderQueue(true)} ><i><AiOutlinePlusCircle className='icon-five'/></i></button>
-                            {renderQueue ? <AddQueue result={result} />: null}
-                        </Tooltip>
-                </figure>
-            </section>
-        </main>
-        </>
-    )
-};
+  //code on lines 24/25 taken from React navigation: url:https://reactnavigation.org/docs/navigation-prop/
+  const goBack = () => {
+    navigate(-1);
+  };
+
+  return (
+    <>
+      <div className="flex flex-col items-center p-2 overflow-auto">
+        <div className="text-white p-12 text-3xl">Discover new ideas.</div>
+        <div className="text-white pb-12 text-xl">
+          <button className="main-button" onClick={goBack}>
+            Back to Search Results
+          </button>
+        </div>
+        <div className="flex flex-col w-5/6 images text-lg p-4">
+          <div className="flex flex-row">
+            <div className="p-2 place-content-center ml-8">
+              <img
+                className="mt-8 mb-8"
+                src={result.image}
+                alt=""
+                width={400}
+                height={500}
+              ></img>
+              <div>{result.title}</div>
+              <div>{result.author}</div>
+            </div>
+            <div className="flex flex-col gap-y-4 justify-center ml-8 p-2">
+              <div>{result.description}</div>
+              <div>Genre: {result.category}</div>
+              <div>Page Count: {result.pageCount}</div>
+              <div>Publication Date: {result.publishedDate}</div>
+              <div>Publisher: {result.publisher}</div>
+              <div>Cost on Barnes and Noble: </div>
+            </div>
+          </div>
+          <div className="flex justify-center p-2">
+            <AddBrowse result={result} />
+            <Tooltip title="Add to Bookshelf">
+              <button onClick={() => setRender(true)}>
+                <i>
+                  <AiOutlineCheck className="text-lg mr-2" />
+                </i>
+              </button>
+              {render ? <AddLibrary result={result} /> : null}
+            </Tooltip>
+            <Tooltip title="Add to Queue">
+              <button onClick={() => setRenderQueue(true)}>
+                <i>
+                  <AiOutlinePlusCircle className="text-lg" />
+                </i>
+              </button>
+              {renderQueue ? <AddQueue result={result} /> : null}
+            </Tooltip>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
 export default Books;
