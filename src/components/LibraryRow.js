4c2638ad1e4bb3a66414fef4b7b164a486b7af07
Meagan <welchmea@oregonstate.edu>
2024-04-08 11:24:02 -0500

    added collection call for backend reset

--- src/components/LibraryRow.js
+++ src/components/LibraryRow.js
@@ -17,42 +17,34 @@ function LibraryRow({ row }) {
   }
 
   return (
-    <div className="flex text-white">
-    <tr key={row.title}>
+    <tr className="text-white" key={row.title}>
       <td className="flex flex-col items-center mb-4">
         <Link to="/ViewBook" state={row}>
           <img src={row.image} alt="" height={300} width={200}></img>
         </Link>
       </td>
-      <td className="flex flex-col items-center mb-2">
-        {row.title}
-      </td>
-      <td className="flex flex-col items-center mb-2">
-        {row.author[0]}
-      </td>
+      <td className="flex flex-col items-center mb-2">{row.title}</td>
+      <td className="flex flex-col items-center mb-2">{row.author[0]}</td>
       <div className="flex flex-row justify-center">
-      <td className="flex flex-col text-xl">
-        <button className="" onClick={() => setaddFavorite(true)}>
-          <i>
-            <AiOutlineHeart className="mr-4"/>
-          </i>
-        </button>
-        {addFavorite ? <AddFavorite result={row} /> : null}
-      </td>
+        <td className="flex flex-col text-xl">
+          <button className="" onClick={() => setaddFavorite(true)}>
+            <i>
+              <AiOutlineHeart className="mr-4" />
+            </i>
+          </button>
+          {addFavorite ? <AddFavorite result={row} /> : null}
+        </td>
 
-      <td className="flex text-xl">
-        <button onClick={verifyDelete}>
-          <i>
-            <AiFillDelete />
-          </i>
-        </button>
-        {render ? <DeleteDb id={row} /> : null}
-      </td>
+        <td className="flex text-xl">
+          <button onClick={verifyDelete}>
+            <i>
+              <AiFillDelete />
+            </i>
+          </button>
+          {render ? <DeleteDb id={row} /> : null}
+        </td>
       </div>
-
     </tr>
-    </div>
-
   );
 }
 
